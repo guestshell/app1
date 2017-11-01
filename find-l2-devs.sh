07fb88657c341bc8f15942314c63763cb12879d5
irsols-devops <devops@irsols.com>
2017-10-31 23:10:45 -0500

    adding initial debug print

--- find-l2-devs.sh
+++ find-l2-devs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export LAN_IFACE_NAME=`ip addr | grep -i broadcast | awk '{ print $2 }'| sed 's/:/\ /g' | head -1`
+echo "Interface connected to local network is : $LAN_IFACE_NAME"
 arp-scan --interface=$LAN_IFACE_NAME --localnet
